d3022858e28b97fc17154e0d93a6136896862092
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-17 16:41:43 -0300

    Renombramiento de referencias del archivo settings

--- donacion_sangre_dj/wsgi.py
+++ donacion_sangre_dj/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import MediaCling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donacion_sangre_dj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donacion_sangre_dj.settings.local")
 
-application = get_wsgi_application()
+application = MediaCling(get_wsgi_application())
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donacion_sangre_dj.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donacion_sangre_dj.settings.local")
 
     from django.core.management import execute_from_command_line
 
