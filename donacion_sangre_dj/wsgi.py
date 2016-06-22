764c38e65fa5c46dc8570407d26a1c1795c222be
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-21 21:18:57 -0300

    Importación de Cling para i integrar archivos estáticos en la aplicación del wsgi.py del proyecto

--- donacion_sangre_dj/wsgi.py
+++ donacion_sangre_dj/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import MediaCling
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donacion_sangre_dj.settings.local")
 
-application = MediaCling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
