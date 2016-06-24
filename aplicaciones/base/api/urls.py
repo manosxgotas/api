4583dd16b4a162a03325b64056ef3377bf6b00cc
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-06-24 19:30:32 -0300

    Creación de urls y vistas para obtener un listado de todos los grupos sanguineos, tipos de documentos y nacionalidades utilizados para realizar el registro del donante en el frontend

--- aplicaciones/base/api/urls.py
+++ aplicaciones/base/api/urls.py
@@ -18,10 +18,18 @@ from django.conf.urls import include, url
 from .views import (
     DonanteListAPI,
     GrupoSanguineoListAPI,
+    NacionalidadListAPI,
+    TipoDocumentoListAPI
     )
 
 urlpatterns = [
+    # Listados atributos del donante
     url(r'^listado-donantes/$', DonanteListAPI.as_view() , name='listado-donantes'),
+    url(r'^listado-grupos-sanguineos/$', GrupoSanguineoListAPI.as_view() , name='listado-grupos-sanguineos'),
+    url(r'^listado-nacionalidades/$', NacionalidadListAPI.as_view() , name='listado-nacionalidades'),
+    url(r'^listado-tipos-documentos/$', TipoDocumentoListAPI.as_view() , name='listado-tipos-documentos'),
+
+    # Urls de aplicaciones
     url(r'^cuentas/', include('aplicaciones.cuentas.api.urls', namespace='cuentas')),
     url(r'^donantes/', include('aplicaciones.donantes.api.urls', namespace='donantes')),
     url(r'^direcciones/', include('aplicaciones.direcciones.api.urls', namespace='direcciones')),
--- aplicaciones/base/api/views.py
+++ aplicaciones/base/api/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 from .serializers import (
     DonanteSerializer,
     GrupoSanguineoSerializer,
+    NacionalidadSerializer,
+    TipoDocumentoSerializer
     )
 
-from aplicaciones.base.models import Donante, GrupoSanguineo
+from aplicaciones.base.models import (
+    Donante,
+    GrupoSanguineo,
+    Nacionalidad,
+    TipoDocumento
+    )
 
 from rest_framework.generics import ListAPIView, CreateAPIView
 
@@ -18,11 +25,20 @@ from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 
 class DonanteListAPI(ListAPIView):
-
     queryset = Donante.objects.all()
     serializer_class = DonanteSerializer
 
 class GrupoSanguineoListAPI(ListAPIView):
-
+    permission_classes = [AllowAny]
     queryset = GrupoSanguineo.objects.all()
     serializer_class = GrupoSanguineoSerializer
+
+class NacionalidadListAPI(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Nacionalidad.objects.all()
+    serializer_class = NacionalidadSerializer
+
+class TipoDocumentoListAPI(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = TipoDocumento.objects.all()
+    serializer_class = TipoDocumentoSerializer
