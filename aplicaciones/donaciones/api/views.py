cf7ba7c37c10065551e5a77d3575077d9000e6d2
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-07 21:52:09 -0300

    Cambio de serializer utilizado en la vista para dar de alta la donación para evitar conflictos entre el alta y recibir información de la misma.

--- aplicaciones/donaciones/api/views.py
+++ aplicaciones/donaciones/api/views.py
@@ -4,16 +4,19 @@ from rest_framework.permissions import AllowAny
 
 from aplicaciones.base.api.permissions import IsOwner
 
+from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
+
 from aplicaciones.base.models import (
     RegistroDonacion,
     Donacion
     )
 
 from .serializers import (
-   DonacionSerializer,
+   DonacionAltaSerializer,
     )
 
 class DonacionAltaAPI(CreateAPIView):
     permission_classes = [IsOwner]
     queryset = Donacion.objects.all()
-    serializer_class = DonacionSerializer
+    parser_classes = [FormParser, MultiPartParser, JSONParser]
+    serializer_class = DonacionAltaSerializer
