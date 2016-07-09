4a9c5aec64503eaf6b3a23a8a538bf4cbeecc5ac
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-09 20:26:36 -0300

    Creación de url para eliminación de donación.

--- aplicaciones/donaciones/api/urls.py
+++ aplicaciones/donaciones/api/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from .views import (
     DonacionAltaAPI,
+    DonacionEliminarAPI
     )
 
 
 urlpatterns = [
     url(r'^crear/$', DonacionAltaAPI.as_view() , name='alta-donacion'),
+    url(r'^eliminar/(?P<id>\w+)$', DonacionEliminarAPI.as_view() , name='eliminar-donacion'),
 ]
