6cdd8eb191673a01f8a83d5fea72df4eee8eb169
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-09 20:20:35 -0300

    Creación de nuevo permiso para el owner de una donación

--- aplicaciones/base/api/permissions.py
+++ aplicaciones/base/api/permissions.py
@@ -1,7 +1,13 @@
 from rest_framework.permissions import IsAuthenticated, BasePermission
 
 class IsOwner(BasePermission):
-    
+
     def has_object_permission(self, request, view, obj):
         return (obj.usuario == request.user and
             request.method in ['GET', 'PUT', 'PATCH'])
+
+class IsOwnerDonacion(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (obj.registro.donante.usuario == request.user and
+            request.method in ['GET', 'PUT', 'PATCH', 'DELETE'])
