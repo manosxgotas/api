e272c02e8c1f265c34e87658c528bdac0bd43870
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-16 13:19:08 -0300

    Creación de url para la vista 'dias_proxima_donacion'.

--- aplicaciones/donaciones/api/urls.py
+++ aplicaciones/donaciones/api/urls.py
@@ -4,7 +4,8 @@ from .views import (
     DonacionCreateAPI,
     DonacionUpdateAPI,
     DonacionDestroyAPI,
-    DonacionInfoAPI
+    DonacionInfoAPI,
+    dias_proxima_donacion
     )
 
 
@@ -14,4 +15,5 @@ urlpatterns = [
     url(r'^editar/(?P<id>\w+)$', DonacionUpdateAPI.as_view() , name='editar-donacion'),
     url(r'^eliminar/(?P<id>\w+)$', DonacionDestroyAPI.as_view() , name='eliminar-donacion'),
     url(r'^(?P<id>\w+)$', DonacionInfoAPI.as_view() , name='info-donacion'),
+    url(r'^proxima-donacion/(?P<usuario_id>\w+)$', dias_proxima_donacion , name='dias-proxima-donacion'),
 ]
