76ff8339931c7207333ebea64544f9bdef4e3595
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-18 15:23:59 -0300

    Se agrego validación 'min_value' para evitar números nulos y negativos en el campo cantidad del formulario para generar códigos de verificación.

--- aplicaciones/base/admin/forms.py
+++ aplicaciones/base/admin/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class FormularioGenerarCodigos(forms.Form):
-    cantidad_codigos = forms.IntegerField(label='Cantidad')
+    cantidad_codigos = forms.IntegerField(min_value=1, label='Cantidad')
     dias_vigencia = forms.IntegerField(min_value=1, required=False,
         label='Días de vigencia a partir de hoy (por defecto 30 días)')
