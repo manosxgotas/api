657d9d16a245d0f97c03381296cea3983e2231e3
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-18 00:14:20 -0300

    Creación de formulario para generar códigos de verificación aleatorios.

---
+++ aplicaciones/base/admin/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class FormularioGenerarCodigos(forms.Form):
+    cantidad_codigos = forms.IntegerField(label='Cantidad')
+    dias_vigencia = forms.IntegerField(min_value=1, required=False,
+        label='Días de vigencia a partir de hoy (por defecto 30 días)')
