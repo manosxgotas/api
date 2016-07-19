d67ee9209e07feead9685b02f38ed2403896f5e8
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-07-19 00:27:42 -0300

    Creación de vista y url para verificar una donación mediante un código ingresado y evaluando si el código existe en la base de datos, si la fecha de la donación es igual o posterior a la fecha de emisión del código y si la donación no se encuentra verificada.

--- aplicaciones/donaciones/api/urls.py
+++ aplicaciones/donaciones/api/urls.py
@@ -5,15 +5,45 @@ from .views import (
     DonacionUpdateAPI,
     DonacionDestroyAPI,
     DonacionInfoAPI,
-    dias_proxima_donacion
+    dias_proxima_donacion,
+    verificar_codigo_donacion
     )
-
 
 urlpatterns = [
 
-    url(r'^crear/$', DonacionCreateAPI.as_view() , name='crear-donacion'),
-    url(r'^editar/(?P<id>\w+)$', DonacionUpdateAPI.as_view() , name='editar-donacion'),
-    url(r'^eliminar/(?P<id>\w+)$', DonacionDestroyAPI.as_view() , name='eliminar-donacion'),
-    url(r'^(?P<id>\w+)$', DonacionInfoAPI.as_view() , name='info-donacion'),
-    url(r'^proxima-donacion/(?P<usuario_id>\w+)$', dias_proxima_donacion , name='dias-proxima-donacion'),
-]
+    url(
+        r'^crear/$',
+        DonacionCreateAPI.as_view(),
+        name='crear-donacion'
+        ),
+
+    url(
+        r'^editar/(?P<id>\w+)$',
+        DonacionUpdateAPI.as_view(),
+        name='editar-donacion'
+        ),
+
+    url(
+        r'^eliminar/(?P<id>\w+)$',
+        DonacionDestroyAPI.as_view(),
+        name='eliminar-donacion'
+        ),
+
+    url(
+        r'^(?P<id>\w+)$',
+        DonacionInfoAPI.as_view(),
+        name='info-donacion'
+        ),
+
+    url(
+        r'^proxima-donacion/(?P<usuario_id>\w+)$',
+        dias_proxima_donacion,
+        name='dias-proxima-donacion'
+        ),
+
+    url(
+        r'^verificar-codigo/(?P<donacion_id>\w+)$',
+        verificar_codigo_donacion,
+        name='verificar-codigo-donacion'
+        ),
+    ]
--- aplicaciones/donaciones/api/views.py
+++ aplicaciones/donaciones/api/views.py
@@ -7,19 +7,23 @@ from rest_framework.generics import (
     RetrieveAPIView
     )
 
-from rest_framework.permissions import AllowAny
-
-from aplicaciones.base.api.permissions import IsOwnerDonacion
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST
+    )
 
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from aplicaciones.base.api.permissions import IsOwnerDonacion
+
 from aplicaciones.base.models import (
-    RegistroDonacion,
+    CodigoVerificacion,
     Donante,
     Donacion,
+    EstadoDonacion,
+    HistoricoEstadoDonacion,
     DIAS_DONACION_POR_GENERO
     )
 
@@ -28,11 +32,13 @@ from .serializers import (
     DonacionUpdateSerializer,
     )
 
+
 class DonacionCreateAPI(CreateAPIView):
     permission_classes = [IsOwnerDonacion]
     queryset = Donacion.objects.all()
     parser_classes = [FormParser, MultiPartParser, JSONParser]
     serializer_class = DonacionCreateSerializer
+
 
 class DonacionUpdateAPI(UpdateAPIView):
     permission_classes = [IsOwnerDonacion]
@@ -40,11 +46,13 @@ class DonacionUpdateAPI(UpdateAPIView):
     serializer_class = DonacionUpdateSerializer
     lookup_field = 'id'
 
+
 class DonacionDestroyAPI(DestroyAPIView):
     permission_classes = [IsOwnerDonacion]
     queryset = Donacion.objects.all()
     serializer_class = DonacionCreateSerializer
     lookup_field = 'id'
+
 
 class DonacionInfoAPI(RetrieveAPIView):
     permission_classes = [IsOwnerDonacion]
@@ -65,16 +73,20 @@ def dias_proxima_donacion(request, usuario_id):
     # Si el donante posee donaciones
     if donaciones_donante:
 
-        # Obtengo la fecha y hora de la última donación realizada por el donante.
+        # Obtengo la fecha y hora de la última donación
+        # realizada por el donante.
         ultima_donacion = donaciones_donante.last().fechaHora
 
         # Obtengo el género del donante.
         genero_donante = donante.genero
 
-        # Calculo la diferencia de días entre la última donación realizada por el donante y la fecha actual.
+        # Calculo la diferencia de días entre la última donación
+        # realizada por el donante y la fecha actual.
         dias_desde_ultima_donacion = datetime.datetime.now() - ultima_donacion
 
-        # Dependiendo del sexo del donante obtengo la cantidad de días necesarios para que el donante pueda realizar una nueva donación.
+        # Dependiendo del sexo del donante obtengo la cantidad
+        # de días necesarios para que el donante pueda realizar.
+        # una nueva donación.
         dias_proxima_donacion = DIAS_DONACION_POR_GENERO[genero_donante] - dias_desde_ultima_donacion.days
 
         # Si la espera en días se cumplió, establezco la cantidad de días en 0.
@@ -82,3 +94,57 @@ def dias_proxima_donacion(request, usuario_id):
 
     # Si no posee donaciones retorno 0, es decir que está apto para donar.
     return Response({"dias": 0})
+
+
+@api_view(['POST'])
+def verificar_codigo_donacion(request, donacion_id):
+    # Obtengo codigo ingresado
+    codigo_ingresado = CodigoVerificacion.objects.filter(codigo=request.data['codigo'])
+
+    # Si existe en la base de datos
+    if codigo_ingresado.exists():
+        donacion = Donacion.objects.get(id=donacion_id)
+
+        # Verifico que la fecha de la donación sea posterior a la fecha
+        # de emisión del código.
+        if donacion.fechaHora.date() >= codigo_ingresado[0].fechaEmision:
+            ultimo_historico_donacion = donacion.historicoEstados.last()
+
+            # Compruebo que la donación no se encuentre verificada.
+            if ultimo_historico_donacion.estado.nombre != 'Verificada':
+                # Establezco fecha fin del último histórico de la donación
+                ultimo_historico_donacion.fin = datetime.datetime.now()
+                ultimo_historico_donacion.save()
+                estado = EstadoDonacion.objects.get(nombre='Verificada')
+
+                # Creo nuevo histórico
+                HistoricoEstadoDonacion.objects.create(
+                    inicio=datetime.datetime.now(),
+                    estado=estado,
+                    donacion=donacion
+                    )
+
+                # Borro código de la base de datos.
+                codigo_ingresado.delete()
+
+                return Response(
+                    {"mensaje": '¡Verificación exitosa!'},
+                    status=HTTP_200_OK
+                    )
+
+            else:
+                return Response(
+                    {"mensaje": 'Esta donación ya ha sido verificada anteriormente.'},
+                    status=HTTP_400_BAD_REQUEST
+                    )
+
+        else:
+            return Response(
+                {"mensaje": 'No puedes utilizar el código introducido debido a que la donación es anterior a la emisión del mismo.'},
+                status=HTTP_400_BAD_REQUEST
+                )
+
+    return Response(
+        {"mensaje": 'El código ingresado es incorrecto o ya ha sido utilizado.'},
+        status=HTTP_400_BAD_REQUEST
+        )
