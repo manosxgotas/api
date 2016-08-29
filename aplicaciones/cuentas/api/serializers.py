73ad404fec92dfc40a0a531344254261d3019124
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-08-29 17:29:58 -0300

    Encapsulamiento de método para enviar mail de confirmación al usuario y generación de clave de activación utilzando el mismo método que generación de link.

--- aplicaciones/cuentas/api/serializers.py
+++ aplicaciones/cuentas/api/serializers.py
@@ -1,31 +1,28 @@
-import datetime
-
 from rest_framework.serializers import (
     ModelSerializer,
-    ValidationError,
-    )
+)
 from aplicaciones.base.models import (
     Donante,
     Direccion,
     RegistroDonacion,
-    )
+)
 
-from django.core.mail import EmailMultiAlternatives
-from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
-from django.template import loader
-from .token import generar_token_confirmacion
+from .token import (
+    enviar_mail_activacion
+)
+
 
 class DireccionRegistroSerializer(ModelSerializer):
 
     class Meta:
         model = Direccion
         fields = [
-        'calle',
-        'numero',
-        'piso',
-        'numeroDepartamento',
-        'localidad',
+            'calle',
+            'numero',
+            'piso',
+            'numeroDepartamento',
+            'localidad',
         ]
 
 User = get_user_model()
@@ -53,17 +50,17 @@ class DonanteRegistroSerializer(ModelSerializer):
     class Meta:
         model = Donante
         fields = [
-        'usuario',
-        'numeroDocumento',
-        'tipoDocumento',
-        'nacimiento',
-        'telefono',
-        'peso',
-        'altura',
-        'genero',
-        'grupoSanguineo',
-        'direccion',
-        'nacionalidad'
+            'usuario',
+            'numeroDocumento',
+            'tipoDocumento',
+            'nacimiento',
+            'telefono',
+            'peso',
+            'altura',
+            'genero',
+            'grupoSanguineo',
+            'direccion',
+            'nacionalidad'
         ]
 
     def create(self, validated_data):
@@ -83,7 +80,8 @@ class DonanteRegistroSerializer(ModelSerializer):
         usuario_data = validated_data.pop('usuario')
         usuario = User(**usuario_data)
         usuario.is_active = False
-        # Seteo la password mediante el método set_password para que la misma sea encriptada
+        # Seteo la password mediante el método set_password
+        # para que la misma sea encriptada.
         usuario.set_password(usuario_data['password'])
         usuario.save()
 
@@ -94,50 +92,22 @@ class DonanteRegistroSerializer(ModelSerializer):
 
         # Creación de la instancia de Donante
         donante_obj = Donante.objects.create(
-            usuario = usuario,
-            claveActivacion = claveActivacion,
-            vencimientoClaveActivacion = vencimientoClaveActivacion,
-            numeroDocumento = numeroDocumento,
-            tipoDocumento = tipoDocumento,
-            nacimiento = nacimiento,
-            telefono = telefono,
-            peso = peso,
-            altura = altura,
-            genero = genero,
-            grupoSanguineo = grupoSanguineo,
-            direccion = direccion,
-            nacionalidad = nacionalidad
-            )
+            usuario=usuario,
+            numeroDocumento=numeroDocumento,
+            tipoDocumento=tipoDocumento,
+            nacimiento=nacimiento,
+            telefono=telefono,
+            peso=peso,
+            altura=altura,
+            genero=genero,
+            grupoSanguineo=grupoSanguineo,
+            direccion=direccion,
+            nacionalidad=nacionalidad
+        )
 
         # Creación de instancia de Registro de donación asociada con la instancia de Donante
         RegistroDonacion.objects.create(donante=donante_obj)
 
-        # Se genera token con email del usuario.
-        token = generar_token_confirmacion(usuario.email)
-        # Creación de URL de confirmación
-        confirm_url = reverse('base:cuentas:activar-cuenta-link', kwargs={'token': token})
-
-        # Obtención de templates html y txt de emails.
-        htmly = loader.get_template('emails/html/confirmar_cuenta.html')
-        text = loader.get_template('emails/txt/confirmar_cuenta.txt')
-
-        # Definición de variables de contexto
-        variables = {
-            'usuario': usuario,
-            'confirm_url': confirm_url,
-            'clave': claveActivacion
-            }
-        html_content = htmly.render(variables)
-        text_content = text.render(variables)
-
-        # Creación y envío de email.
-        msg = EmailMultiAlternatives(
-            'Bienvenido a Manos por gotas',
-            text_content,
-            to=[usuario.email]
-            )
-
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+        enviar_mail_activacion(usuario)
 
         return donante_obj
--- aplicaciones/cuentas/api/token.py
+++ aplicaciones/cuentas/api/token.py
@@ -1,31 +1,76 @@
-from itsdangerous import URLSafeTimedSerializer
+from itsdangerous import (
+    URLSafeTimedSerializer,
+    TimedSerializer
+)
 from manosxgotas.settings.local import (
     SECRET_KEY,
-    SECURITY_PASSWORD_SALT
-    )
+    SECURITY_PASSWORD_SALT,
+    FRONTEND_URL
+)
+from django.template import loader
+from django.core.mail import EmailMultiAlternatives
 
 
-def generar_token_confirmacion(email):
+def generar_token_confirmacion(key, url=False):
     '''
     Generación de serializer parseado mediante la clave privada
-    de la app y se obtiene el token a partir del email y firmado
-    con la clave privada 'SALT'.
+    de la app y obtención del token a partir del email/id firmado
+    con la clave de seguridad 'SALT'.
     '''
-    serializer = URLSafeTimedSerializer(SECRET_KEY)
-    return serializer.dumps(email, salt=SECURITY_PASSWORD_SALT)
+    if url:
+        serializer = URLSafeTimedSerializer(SECRET_KEY)
+    else:
+        serializer = TimedSerializer(SECRET_KEY)
+    return serializer.dumps(key, salt=SECURITY_PASSWORD_SALT)
 
-def confirmar_token(token, vencimiento=10800):
+
+def confirmar_token(token, url=False, vencimiento=43200):
     '''
-    Proceso inverso para obtener email a confirmar mediante
+    Proceso inverso para obtener email/id a confirmar mediante
     el procesamiento del token.
     '''
-    serializer = URLSafeTimedSerializer(SECRET_KEY)
+    if url:
+        serializer = URLSafeTimedSerializer(SECRET_KEY)
+    else:
+        serializer = TimedSerializer(SECRET_KEY)
     try:
-        email = serializer.loads(
+        key = serializer.loads(
             token,
             salt=SECURITY_PASSWORD_SALT,
             max_age=vencimiento
-            )
+        )
     except:
         return False
-    return email
+    return key
+
+
+def enviar_mail_activacion(usuario):
+    # Se genera token con email del usuario.
+    token_link = generar_token_confirmacion(usuario.email, url=True)
+    token_clave = generar_token_confirmacion(usuario.id)
+
+    # Creación de URL de confirmación
+    confirm_url = FRONTEND_URL + 'activar-cuenta-link/' + token_link
+
+    # Obtención de templates html y txt de emails.
+    htmly = loader.get_template('emails/html/confirmar_cuenta.html')
+    text = loader.get_template('emails/txt/confirmar_cuenta.txt')
+
+    # Definición de variables de contexto
+    variables = {
+        'usuario': usuario,
+        'confirm_url': confirm_url,
+        'clave': token_clave
+    }
+    html_content = htmly.render(variables)
+    text_content = text.render(variables)
+
+    # Creación y envío de email.
+    msg = EmailMultiAlternatives(
+        'Bienvenido a Manos por gotas',
+        text_content,
+        to=[usuario.email]
+    )
+
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
