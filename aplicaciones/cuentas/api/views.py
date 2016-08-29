7e738ce6b6ef2dde45f12de5d161cc135df5d311
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-08-29 17:31:17 -0300

    Corrección de view para activar cuenta de usuario, decodificación de clave y link de confirmación mediante itsdangerous.

--- aplicaciones/cuentas/api/views.py
+++ aplicaciones/cuentas/api/views.py
@@ -1,7 +1,10 @@
 import datetime
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+    api_view,
+    permission_classes
+    )
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -26,6 +29,7 @@ class DonanteRegistroAPI(CreateAPIView):
 
 
 @api_view(['POST', 'GET'])
+@permission_classes((AllowAny, ))
 def activar_cuenta(request, token=None):
     '''
     Activación de cuenta mediante acceso a link (GET) o mediante
@@ -33,45 +37,31 @@ def activar_cuenta(request, token=None):
     se encuentren vencidos y que el usuario no se encuentre ya
     activo.
     '''
+    Usuario = get_user_model()
     if request.method == 'GET':
-        Usuario = get_user_model()
-        try:
-            email = confirmar_token(token)
-        except:
-            return Response(
-                {"mensaje": 'El link de confirmación es inválido o ha vencido.'},
-                status=HTTP_400_BAD_REQUEST
-                )
-        usuario = get_object_or_404(Usuario, email=email)
+        key = confirmar_token(token, url=True)
+    else:
+        key = confirmar_token(request.data['clave'])
+
+    if key is False:
+        return Response(
+            {"mensaje": 'El link de confirmación es inválido o ha vencido.'},
+            status=HTTP_400_BAD_REQUEST
+        )
+    else:
+        if request.method == 'GET':
+            usuario = get_object_or_404(Usuario, email=key)
+        else:
+            usuario = get_object_or_404(Usuario, id=key)
         if usuario.is_active:
             return Response(
                 {"mensaje": 'Esta cuenta ya se encuentra activada.'},
                 status=HTTP_400_BAD_REQUEST
-                )
+            )
         else:
             usuario.is_active = True
             usuario.save()
             return Response(
                 {"mensaje": 'Se ha activado correctamente tu cuenta de registro. ¡Gracias!'},
                 status=HTTP_200_OK
-                )
-    else:
-        donante = get_object_or_404(Donante, claveActivacion=request.data['clave'])
-        if donante.usuario.is_active is False:
-            if datetime.datetime.now() > donante.vencimientoClaveActivacion:
-                return Response(
-                    {"mensaje": 'El código ingresado es incorrecto o ya ha sido utilizado.'},
-                    status=HTTP_400_BAD_REQUEST
-                    )
-            else:
-                donante.usuario.is_active = True
-                donante.usuario.save()
-                return Response(
-                    {"mensaje": 'Se ha activado correctamente tu cuenta de registro.'},
-                    status=HTTP_200_OK
-                    )
-        else:
-            return Response(
-                {"mensaje": 'Esta cuenta ya se encuentra activada.'},
-                status=HTTP_400_BAD_REQUEST
-                )
+            )
