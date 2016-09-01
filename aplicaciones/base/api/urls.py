eba61a90680ca9150656702f8e2b74a8cfba7994
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-09-01 12:06:24 -0300

    Creación de vista y url para obtener el detalle de un centro de donación mediante su id y cambio del serializer CentroDonacion para obtener los horarios del mismo.

--- aplicaciones/base/api/urls.py
+++ aplicaciones/base/api/urls.py
@@ -1,22 +1,8 @@
-"""manosxgotas URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 
 from .views import (
     CentroDonacionListAPI,
+    CentroDonacionInfoAPI,
     DonanteListAPI,
     EventoListAPI,
     EventoInfoAPI,
@@ -32,6 +18,7 @@ urlpatterns = [
     url(r'^listado-nacionalidades/$', NacionalidadListAPI.as_view(), name='listado-nacionalidades'),
     url(r'^listado-tipos-documentos/$', TipoDocumentoListAPI.as_view(), name='listado-tipos-documentos'),
     url(r'^listado-centros-donacion/$', CentroDonacionListAPI.as_view(), name='listado-centros-donacion'),
+    url(r'^centro/(?P<id>\w+)$', CentroDonacionInfoAPI.as_view(), name='detalle-centro'),
     url(r'^evento/(?P<id>\w+)$', EventoInfoAPI.as_view(), name='detalle-evento'),
     url(r'^listado-eventos/$', EventoListAPI.as_view(), name='listado-eventos'),
 
