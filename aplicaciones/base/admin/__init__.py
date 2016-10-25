d164dfd4772464d9a853af3bb5fc5ce4c5f9f9c2
Martin Solas <martin_94_mza@hotmail.com>
2016-10-24 21:18:47 -0300

    Seteo de las variables en admin site para modificar title y header de la pagina de administración

--- aplicaciones/base/admin/__init__.py
+++ aplicaciones/base/admin/__init__.py
@@ -16,3 +16,6 @@ from .solicitudDonacion import *
 from .tipoCentroDonacion import *
 from .tipoDocumento import *
 from .tipoSolicitudDonacion import *
+
+admin.site.site_header = 'Administración de Manos por gotas'
+admin.site.site_title = 'Administración de Manos por gotas'
