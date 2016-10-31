9704b52a7989f0b0bd6a9722df1183341ca4cab8
Renzo Manganiello <renzo@fmanganiello.com.ar>
2016-10-31 15:23:39 -0300

    Creación de permiso IsOwnerSolicitud

--- aplicaciones/base/api/permissions.py
+++ aplicaciones/base/api/permissions.py
@@ -1,13 +1,22 @@
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import BasePermission
+
 
 class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (obj.usuario == request.user and
-            request.method in ['GET', 'PUT', 'PATCH'])
+                request.method in ['GET', 'PUT', 'PATCH'])
+
 
 class IsOwnerDonacion(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (obj.registro.donante.usuario == request.user and
-            request.method in ['GET', 'PUT', 'PATCH', 'DELETE'])
+                request.method in ['GET', 'PUT', 'PATCH', 'DELETE'])
+
+
+class IsOwnerSolicitud(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (obj.donante.usuario == request.user and
+                request.method in ['GET', 'PUT', 'PATCH', 'DELETE'])
